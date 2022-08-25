fc0877b50e57dd91eafa1468aa54a96a9352d18f
olenkasereda <106010289+olenkasereda@users.noreply.github.com>
2022-08-25 21:26:45 +0300

    arrays exemples

---
+++ repeta_tasks.js
@@ -0,0 +1,48 @@
+/*  02-payment.js
+ * Посчитать общую сумму покупок в корзине
+ */
+
+// const cart = [54, 28, 105, 70, 92, 17, 120, 12, 25, 90];
+// // 2 сделать переменную total до цикла
+// let total = 0;
+
+// for (let i = 0; i < cart.length; i += 1) {
+//     total += cart[i];
+// }
+// console.log('total=', total);
+
+/* or */
+
+// for (const value of cart) {
+//   total += value;
+// }
+
+// console.log('Total: ', total);
+
+
+// Добавить такс
+
+// for (let i = 0; i < cart.length; i += 1) {
+//   cart[i] = Math.round(cart[i] * 1.1);
+// }
+
+// // for (let value of cart) {
+// //   value = Math.round(value * 1.1);
+// // }
+
+// console.log(cart);
+
+// =====================================================
+
+/*  even.js
+ * Напиши скрипт который подсчитывает сумму всех чётных чисел в массиве.
+ */
+
+const numbers = [1, 5, 8, 9, 12, 4, 15, 27, 30, 18, 14];
+let total = 0;
+
+for (let value of numbers) {
+  value = numbers[i];
+}
+
+console.log(cart);
