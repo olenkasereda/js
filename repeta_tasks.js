725e8e688687e3b9be02740b335fa2b621b9c1d5
olenkasereda <106010289+olenkasereda@users.noreply.github.com>
2022-08-29 21:21:38 +0300

    work in terminal

--- repeta_tasks.js
+++ repeta_tasks.js
@@ -38,11 +38,66 @@
  * Напиши скрипт который подсчитывает сумму всех чётных чисел в массиве.
  */
 
-const numbers = [1, 5, 8, 9, 12, 4, 15, 27, 30, 18, 14];
-let total = 0;
+// const numbers = [1, 5, 8, 9, 12, 4, 15, 27, 30, 18, 14];
+// let total = 0;
 
-for (let value of numbers) {
-  value = numbers[i];
-}
+// for (let value of numbers) {
+//   value = numbers[i];
+// }
 
-console.log(cart);
+// console.log(cart);
+
+
+
+// =================================================================
+// Напиши функцию getExtremeElements(array)
+// которая принимает один параметр array - массив
+// элементов произвольной длины.Функция должна возвращать
+//  массив из двух элементов - первого и последнего
+//  элемента параметра array.
+
+// function getExtremeElements(array) {
+// return [array[0], array[array.length-1]]
+
+// }
+
+// // ========= #12 ===========================
+// function makeStringFromArray(array, delimeter) {
+//   let string;
+//   string = array.join(delimeter);
+
+//   return console.log(string);
+// }
+
+// makeStringFromArray(["Mango", "hurries", "to", "the", "train"], " ");
+// makeStringFromArray(["M", "a", "n", "g", "o"], "");
+// makeStringFromArray(["top", "picks", "for", "you"], "_");
+
+
+// ========= #16 =================================
+// function makeArray(firstArray, secondArray, maxLength) {
+//     // Change code below this line
+//   const newArray = firstArray.concat(secondArray);
+//   if (newArray.length > maxLength) {
+//    return console.log(newArray.slice(0, maxLength));
+          
+//   } else {
+//       return console.log(newArray);
+//   }
+// }
+/* или второй вариант*/
+
+// function makeArray(firstArray, secondArray, maxLength) {
+//     // Change code below this line
+//  const newArray = firstArray.concat(secondArray);
+//  const result = newArray.slice(0, maxLength);
+//   return  result;
+// }
+
+// makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3);
+// makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4);
+// makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3);
+// makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2);
+// makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4);
+// makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0);
+
